a13e0810e66ffda8617a1c24f4748ef0344bd24c
Alex Alemi <alemi@google.com>
2019-11-21 19:03:35 -0800

    fixing times

--- src/rss.py
+++ src/rss.py
@@ -3,6 +3,7 @@
 
 import json
 import logging
+import datetime
 import os
 from feedgen.feed import FeedGenerator
 
@@ -16,31 +17,43 @@ BUILD_PATH = "../publications"
 PDF_PATH = "https://alexalemi.com/publications/"
 
 
-fg = FeedGenerator()
-fg.id("https://alexalemi.com/publications/rss.xml")
-fg.title("Alexander A. Alemi's Publications")
-fg.author({"name": "Alexander A. Alemi", "email": "alexalemi@gmail.com"})
-fg.link(href="https://alexalemi.com", rel="alternate")
-fg.logo("http://alexalemi.com/favicon.ico")
-fg.subtitle("Follow my publications and talks.")
-fg.link(href="https://alexalemi.com/publications/rss.xml", rel="self")
-fg.language("en")
+def convert_date(s):
+  return datetime.datetime.strptime(s, "%Y-%m").astimezone()
 
-with open(os.path.join(DATA_PATH, "research.json"), 'r') as f:
-    data = json.load(f)
+def annotate(fe, publication):
+  fe.title(pub.get('title'))
+  fe.published(convert_date(pub.get('date')))
 
-    for pub in data:
-      fe = fg.add_entry()
+  if pub.get('file'):
+    pdf_path = os.path.join(PDF_PATH, pub.get("file"))
+    fe.id(pdf_path)
+    fe.link(href=pdf_path)
+    fe.enclosure(pdf_path, 0, "application/pdf")
+  elif pub.get('link'):
+    href = pub.get('link').get('href')
+    fe.id(href)
+    fe.link(href=href)
+  return fe
 
-      if pub.get('file'):
-        pdf_path = os.path.join(PDF_PATH, pub.get("file"))
-        fe.id(pdf_path)
-        fe.title(pub.get('title'))
-        fe.enclosure(pdf_path, 0, "application/pdf")
-      elif pub.get('link'):
-        fe.id(pub.get('link').get('href'))
-        fe.title(pub.get('title'))
+
+if __name__ == "__main__":
+  fg = FeedGenerator()
+  fg.id("https://alexalemi.com/publications/rss.xml")
+  fg.title("Alexander A. Alemi's Publications")
+  fg.author({"name": "Alexander A. Alemi", "email": "alexalemi@gmail.com"})
+  fg.link(href="https://alexalemi.com", rel="alternate")
+  fg.logo("http://alexalemi.com/favicon.ico")
+  fg.subtitle("Follow my publications and talks.")
+  fg.link(href="https://alexalemi.com/publications/rss.xml", rel="self")
+  fg.language("en")
+
+
+  with open(os.path.join(DATA_PATH, "research.json"), 'r') as f:
+      data = json.load(f)
+
+      for pub in data:
+        fe = fg.add_entry()
+        annotate(fe, pub)
 
 
 rssfeed = fg.rss_file(os.path.join(BUILD_PATH, RSS_FILENAME))
-
