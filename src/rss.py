19019d80b917330f5927eace43c7a7c8792c03e8
Alex Alemi <alemi@google.com>
2019-11-21 19:44:08 -0800

    with rss feed

--- src/rss.py
+++ src/rss.py
@@ -13,47 +13,67 @@ logging.basicConfig(level=logging.DEBUG)
 ROOT = os.path.dirname(os.path.realpath(__file__))
 RSS_FILENAME = "rss.xml"
 DATA_PATH = "../data"
-BUILD_PATH = "../publications"
-PDF_PATH = "https://alexalemi.com/publications/"
+BUILD_PATH = "../"
+PDF_PATH = "../publications/"
+PDF_SCHEME = "https://alexalemi.com/publications/"
+TALK_SCHEME = "https://alexalemi.com/talks/"
 
 
 def convert_date(s):
   return datetime.datetime.strptime(s, "%Y-%m").astimezone()
 
-def annotate(fe, publication):
-  fe.title(pub.get('title'))
-  fe.published(convert_date(pub.get('date')))
+def add_publication(fe, publication):
+  fe.title(pub["title"])
+  fe.published(convert_date(pub["date"]))
+  fe.category(term="publications", scheme=PDF_PATH, label="publications")
 
   if pub.get('file'):
-    pdf_path = os.path.join(PDF_PATH, pub.get("file"))
-    fe.id(pdf_path)
+    pdf_path = os.path.join(PDF_SCHEME, pub["file"])
+    fe.guid(pdf_path, permalink=True)
     fe.link(href=pdf_path)
-    fe.enclosure(pdf_path, 0, "application/pdf")
+    size = os.path.getsize(os.path.join(PDF_PATH, pub["file"]))
+    fe.enclosure(pdf_path, str(size), "application/pdf")
   elif pub.get('link'):
-    href = pub.get('link').get('href')
-    fe.id(href)
+    href = pub["link"]["href"]
+    if pub.get("id"):
+      fe.guid(os.path.join(PDF_SCHEME, pub["id"]), permalink=True)
+    else:
+      fe.guid(href)
     fe.link(href=href)
+  return fe
+
+def add_talk(fe, talk):
+  fe.title(talk["title"])
+  fe.guid(talk["id"], permalink=True)
+  fe.published(convert_date(talk["date"]))
+  fe.category(term="talks", scheme=TALK_SCHEME, label="talks")
+  fe.link(href=talk["link"]["href"])
   return fe
 
 
 if __name__ == "__main__":
   fg = FeedGenerator()
-  fg.id("https://alexalemi.com/publications/rss.xml")
+  fg.id("https://alexalemi.com/rss.xml")
   fg.title("Alexander A. Alemi's Publications")
   fg.author({"name": "Alexander A. Alemi", "email": "alexalemi@gmail.com"})
   fg.link(href="https://alexalemi.com", rel="alternate")
   fg.logo("http://alexalemi.com/favicon.ico")
   fg.subtitle("Follow my publications and talks.")
-  fg.link(href="https://alexalemi.com/publications/rss.xml", rel="self")
+  fg.link(href="https://alexalemi.com/rss.xml", rel="self")
   fg.language("en")
 
 
   with open(os.path.join(DATA_PATH, "research.json"), 'r') as f:
       data = json.load(f)
-
       for pub in data:
         fe = fg.add_entry()
-        annotate(fe, pub)
+        add_publication(fe, pub)
+
+  with open(os.path.join(DATA_PATH, "talks.json"), 'r') as f:
+      data = json.load(f)
+      for talk in data:
+        fe = fg.add_entry()
+        add_talk(fe, talk)
 
 
 rssfeed = fg.rss_file(os.path.join(BUILD_PATH, RSS_FILENAME))
